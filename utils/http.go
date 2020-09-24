d95c564b1b7bb25c8fffad0e88867ea224d4cd75
ttlv <gogophersteven@gmail.com>
2020-09-24 14:30:58 +0800

    feat/http: refactor api get function

--- utils/http.go
+++ utils/http.go
@@ -57,21 +57,31 @@ func Post(url string, body interface{}, params url.Values, headers map[string]st
 	return "", err
 }
 
-func Get(rawUrl string, params url.Values) (result string, err error) {
-	var (
-		Url     *url.URL
-		urlPath string
-	)
-	if params != nil {
-		if Url, err = url.Parse(rawUrl); err != nil {
-			return "", err
-		}
-		Url.RawQuery = params.Encode()
-		urlPath = Url.String()
-	} else {
-		urlPath = rawUrl
+func Get(url string, params url.Values, headers map[string]string) (string, error) {
+	//add post body
+	var req *http.Request
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		log.Println(err)
+		return "", errors.New("new request is fail: %v \n")
 	}
-	resp, err := http.Get(urlPath)
+	//add params
+	q := req.URL.Query()
+	if params != nil {
+		for key, val := range params {
+			q.Add(key, val[0])
+		}
+		req.URL.RawQuery = q.Encode()
+	}
+	//add headers
+	if headers != nil {
+		for key, val := range headers {
+			req.Header.Add(key, val)
+		}
+	}
+	//http client
+	client := &http.Client{}
+	resp, err := client.Do(req)
 	if resp != nil {
 		defer resp.Body.Close()
 		body, err := ioutil.ReadAll(resp.Body)
